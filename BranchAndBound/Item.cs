05e33a92b8126089e588e3ca32b9463a5bfdfe35
DMITRIJ MAMAJEV <mokelok@live.ru>
2016-04-13 23:21:18 +0300

    First commit

---
+++ BranchAndBound/Item.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchAndBound
+{
+    public class Item : IComparable<Item>
+    {
+        public int ID { get; set; }
+        public int Weight { get; set; }
+        public int Profit { get; set; }
+
+        public decimal Efficiency { get; set; }
+
+        public Item(int id, int weight, int profit)
+        {
+            this.ID = id;
+            this.Profit = profit;
+            this.Weight = weight;
+            this.Efficiency = Convert.ToDecimal(profit)/Convert.ToDecimal(weight);
+        }
+
+        public int CompareTo(Item other)
+        {
+            if (this.Profit > other.Profit)
+                return -1;
+            else
+            {
+                return 1;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"Item Id: {this.ID}, Weight: {this.Weight}, Profit: {this.Profit}, Efficiency: {this.Efficiency}";
+        }
+    }
+}
---
+++ BranchAndBound/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchAndBound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> knapsackItemsList = new List<Item>()
+            {
+                new Item(1, 32, 727),
+                new Item(2, 40, 763),
+                new Item(3, 44, 60),
+                new Item(4, 20, 606),
+                new Item(5, 1,  45),
+                new Item(6, 29, 370),
+                new Item(7, 3,  414),
+                new Item(8, 13, 880),
+                new Item(9, 6,  133),
+                new Item(10, 39, 820)
+            };
+
+            knapsackItemsList.Sort();
+            foreach (Item i in knapsackItemsList)
+            {
+                Console.WriteLine(i.ToString());
+            }    
+        }
+
+        
+    }
+}
